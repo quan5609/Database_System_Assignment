92aa47f60fc6fe14b897e01aebcd03a0e23d0b04
Quan Bui <quan.buicompscibk@hcmut.edu.vn>
2020-11-24 21:00:50 +0700

    Final Commit

--- Assignment_1/SQLscripts/users_role.sql
+++ Assignment_1/SQLscripts/users_role.sql
@@ -24,39 +24,73 @@ CREATE USER deemployee FOR LOGIN deemployee
 --Create teacher user
 CREATE USER teacher FOR LOGIN teacher
 
+--Create so role
+CREATE ROLE students 
+
+--Create students role
+CREATE ROLE soemployees 
+
+--Create de role
+CREATE ROLE deemployees
+
+--Create teacher role
+CREATE ROLE teachers
+
+--Add student
+ALTER ROLE  students  
+	ADD MEMBER student;
+
+--Add so
+ALTER ROLE  soemployees  
+	ADD MEMBER soemployee;
+
+--Add de
+ALTER ROLE  deemployees  
+	ADD MEMBER deemployee;
+
+--Add teacher
+ALTER ROLE  teachers  
+	ADD MEMBER teacher;  
+ 
 
 --students permission
-GRANT ALL ON OBJECT::Register TO student
-GRANT SELECT ON OBJECT::[Subject] TO student
-GRANT SELECT ON OBJECT::Class TO student
-GRANT SELECT ON OBJECT::Opens TO student
-GRANT SELECT ON OBJECT::Responsible TO student
-GRANT SELECT ON OBJECT::StudyStatus TO student
-GRANT SELECT ON OBJECT::Uses TO student
+GRANT ALL ON OBJECT::Register TO students
+GRANT SELECT ON OBJECT::[Subject] TO students
+GRANT SELECT ON OBJECT::Class TO students
+GRANT SELECT ON OBJECT::Opens TO students
+GRANT SELECT ON OBJECT::Responsible TO students
+GRANT SELECT ON OBJECT::StudyStatus TO students
+GRANT SELECT ON OBJECT::Uses TO students
+GRANT SELECT ON OBJECT::ReferenceBook TO students
+GRANT SELECT ON OBJECT::Author TO students
+GRANT SELECT ON OBJECT::Field TO students
+GRANT SELECT ON OBJECT::Teacher TO students
+GRANT SELECT ON OBJECT::MainTeacher TO students
+GRANT SELECT ON OBJECT::Semester TO students
+
 
 --teachers permission
-GRANT ALL ON OBJECT::Uses TO teacher
-GRANT SELECT ON SCHEMA::dbo TO teacher
-DENY SELECT ON OBJECT::Employee TO teacher
+GRANT ALL ON OBJECT::Uses TO teachers
+GRANT SELECT ON SCHEMA::dbo TO teachers
 
 --department employee permission
-GRANT ALL ON OBJECT::Opens TO deemployee
-GRANT ALL ON OBJECT::Responsible TO deemployee
-GRANT ALL ON OBJECT::MainResponsible TO deemployee
-GRANT SELECT ON SCHEMA::dbo TO deemployee
-DENY SELECT ON OBJECT::Employee TO deemployee
+GRANT ALL ON OBJECT::Opens TO deemployees
+GRANT ALL ON OBJECT::Responsible TO deemployees
+GRANT ALL ON OBJECT::MainResponsible TO deemployees
+GRANT SELECT ON SCHEMA::dbo TO deemployees
+
 
 --study office employee permission
-GRANT ALL ON OBJECT::Register TO soemployee
-GRANT ALL ON OBJECT::Class TO soemployee
-GRANT ALL ON OBJECT::StudyStatus TO soemployee
-GRANT SELECT ON SCHEMA::dbo TO soemployee
-DENY SELECT ON OBJECT::Employee TO soemployee
+GRANT ALL ON OBJECT::Register TO soemployees
+GRANT ALL ON OBJECT::Class TO soemployees
+GRANT ALL ON OBJECT::StudyStatus TO soemployees
+GRANT SELECT ON SCHEMA::dbo TO soemployees
 
 
 
-/*
-USE Class_Registration
+
+
+/*USE Class_Registration
 
 SELECT * FROM Employee
 SELECT * FROM sys.database_principals
@@ -65,4 +99,5 @@ DROP ROLE soemployees
 DROP ROLE deemployees
 DROP USER student
 DROP USER soemployee
-DROP USER deemployee*/
+DROP USER deemployee
+Drop USER teacher*/
