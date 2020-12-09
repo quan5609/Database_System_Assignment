98ad8d74ca74672c8e632fd090f3919104a9c0bf
Trung <trung@vidu.com>
2020-12-09 10:43:29 +0700

    Add trigger total for class and main_teacher

--- Assignment_1/SQLscripts/trigger.sql
+++ Assignment_1/SQLscripts/trigger.sql
@@ -69,4 +69,66 @@ BEGIN
 END
 
 -- Check for total participation of Main Teacher 
+DROP TRIGGER IF EXISTS total_participation_main_teacher_1
+GO
+CREATE TRIGGER total_participation_main_teacher_1 ON MainTeacher
+AFTER INSERT
+AS
+BEGIN
+    IF EXISTS
+        ((SELECT ssn FROM MainTeacher)
+        EXCEPT
+        (SELECT DISTINCT MainTeacher_ssn FROM MainResponsible))
+    BEGIN
+        RAISERROR ('Giang vien chinh phai phu trach chinh it nhat mot mon hoc',-1,-1)
+        ROLLBACK TRANSACTION
+    END
+END
+
+DROP TRIGGER IF EXISTS total_participation_main_teacher_2
+GO
+CREATE TRIGGER total_participation_main_teacher_2 ON MainResponsible
+AFTER DELETE, UPDATE
+AS
+BEGIN
+    IF EXISTS
+        ((SELECT ssn FROM MainTeacher)
+        EXCEPT
+        (SELECT DISTINCT MainTeacher_ssn FROM MainResponsible))
+    BEGIN
+        RAISERROR ('Giang vien chinh phai phu trach chinh it nhat mot mon hoc',-1,-1)
+        ROLLBACK TRANSACTION
+    END
+END
 -- Check for total participation of Class
+DROP TRIGGER IF EXISTS total_participation_class_1
+GO
+CREATE TRIGGER total_participation_class_1 ON Class
+AFTER INSERT
+AS
+BEGIN
+    IF EXISTS
+        ((SELECT id, Semester_id, Subject_id FROM Class)
+        EXCEPT
+        (SELECT DISTINCT Class_id, Semester_id, Subject_id FROM Uses))
+    BEGIN
+        RAISERROR ('Lop hoc phai co giao trinh chinh',-1,-1)
+        ROLLBACK TRANSACTION
+    END
+END
+
+DROP TRIGGER IF EXISTS total_participation_class_2
+GO
+CREATE TRIGGER total_participation_class_2 ON Uses
+AFTER DELETE, UPDATE
+AS
+BEGIN
+    IF EXISTS
+        ((SELECT id, Semester_id, Subject_id FROM Class)
+        EXCEPT
+        (SELECT DISTINCT Class_id, Semester_id, Subject_id FROM Uses))
+    BEGIN
+        RAISERROR ('Lop hoc phai co giao trinh chinh',-1,-1)
+        ROLLBACK TRANSACTION
+    END
+END
